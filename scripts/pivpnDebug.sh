516b93ee431d70e27bbcd6e73771922797f336b9
redfast00 <redfast00@users.noreply.github.com>
2016-10-24 19:28:08 +0200

    Added ISSUE_TEMPLATE.md, wrote debug script (#115)

--- scripts/pivpnDebug.sh
+++ scripts/pivpnDebug.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env bash
-echo "::: This feature is not yet implemented... stay tuned!"
+
+# This scripts runs as root
+echo ":: PiVPN Debug ::"
+echo ":: Latest commit ::"
+git --git-dir /etc/.pivpn/.git log -n 1
+echo ":: list of files in /etc/openvpn/easy-rsa/keys ::"
+ls /etc/openvpn/easy-rsa/keys/
+echo ":: /etc/pivpn/* ::"
+for filename in /etc/pivpn/*; do
+    echo ":: START $filename ::"
+    cat "$filename"
+    echo ":: END $filename ::"
+done
+echo ":: /etc/openvpn/easy-rsa/keys/Default.txt ::"
+cat /etc/openvpn/easy-rsa/keys/Default.txt
+echo ":: done ::"
