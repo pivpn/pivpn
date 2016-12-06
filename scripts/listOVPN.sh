9b8a883119de4bf0c94baa402117b2dbece89c64
Kaladin Light <0.kaladin@gmail.com>
2016-12-06 10:56:51 -0500

    pivpn add for easyrsa3, updates to pivpn list for easyrsa3

--- scripts/listOVPN.sh
+++ scripts/listOVPN.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # PiVPN: list clients script
 
-INDEX="/etc/openvpn/easy-rsa/keys/index.txt"
+INDEX="/etc/openvpn/easy-rsa/pki/index.txt"
 printf "\n"
-if [ ! -f "$INDEX" ]; then
+if [ ! -f "${INDEX}" ]; then
         echo "The file: $INDEX was not found!"
         exit 1
 fi
@@ -15,13 +15,13 @@ printf " ::\e[4m  Status  \e[0m||\e[4m   Name   \e[0m:: \n"
 
 while read -r line || [ -n "$line" ]; do
     STATUS=$(echo "$line" | awk '{print $1}')
-    NAME=$(echo "$line" | sed -e 's/^.*CN=\([^/]*\)\/.*/\1/')
-    if [ "$STATUS" = "V" ]; then
+    NAME=$(echo "$line" | sed -e 's:.*/CN=::')
+    if [ "${STATUS}" == "V" ]; then
         printf "     Valid   ::   %s\n" "$NAME"
-    elif [ "$STATUS" = "R" ]; then
+    elif [ "${STATUS}" == "R" ]; then
         printf "     Revoked ::   %s\n" "$NAME"
     else
         printf "     Unknown ::   %s\n" "$NAME"
     fi
-done <$INDEX
+done <${INDEX}
 printf "\n"
