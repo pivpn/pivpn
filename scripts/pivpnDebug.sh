9b937d0e6ba0befb755e19b6173ddb7333b3f008
Kaladin Light <0.kaladin@gmail.com>
2016-12-08 09:53:23 -0500

    Fix pivpnDebug after easy-rsa3 changes

--- scripts/pivpnDebug.sh
+++ scripts/pivpnDebug.sh
@@ -4,14 +4,14 @@
 echo ":: PiVPN Debug ::"
 echo ":: Latest commit ::"
 git --git-dir /etc/.pivpn/.git log -n 1
-echo ":: list of files in /etc/openvpn/easy-rsa/keys ::"
-ls /etc/openvpn/easy-rsa/keys/
+echo ":: Recursive list of files in /etc/openvpn/easy-rsa/pki ::"
+ls -LR /etc/openvpn/easy-rsa/pki/ -Ireqs -Icerts_by_serial
 echo ":: /etc/pivpn/* ::"
 for filename in /etc/pivpn/*; do
     echo ":: START $filename ::"
     cat "$filename"
     echo ":: END $filename ::"
 done
-echo ":: /etc/openvpn/easy-rsa/keys/Default.txt ::"
-cat /etc/openvpn/easy-rsa/keys/Default.txt
+echo ":: /etc/openvpn/easy-rsa/pki/Default.txt ::"
+cat /etc/openvpn/easy-rsa/pki/Default.txt
 echo ":: done ::"
