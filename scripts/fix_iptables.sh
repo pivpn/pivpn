8a0f31b7ca146d1d9040bc1b451aaba0337c059d
Kaladin Light <0.kaladin@gmail.com>
2016-12-11 22:04:17 -0500

    Try some iptables fix. (2/2). Needs unit testing.

--- scripts/fix_iptables.sh
+++ scripts/fix_iptables.sh
@@ -2,4 +2,8 @@
 # PiVPN: Fix iptables script
 # called by pivpnDebug.sh
 
-echo "I don't exist yet..."
+IPv4dev=$(ip route get 8.8.8.8 | awk '{for(i=1;i<=NF;i++)if($i~/dev/)print $(i+1)}')
+iptables -t nat -F
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o ${IPv4dev} -j MASQUERADE
+iptables-save > /etc/iptables/rules.v4
+iptables-restore < /etc/iptables/rules.v4
