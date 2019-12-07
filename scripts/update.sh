a870d04fb618ee79aba9e5989b98c7c1efd35427
corbolais <corbolais@gmail.com>
2019-12-07 22:59:44 +0100

    add missing bang to shebang. correct usage of cp. correct wrong copy target dir. safeguard sudo-rm-rf. safeguard against variable expansion into IFS chars. clean up whitespace.
    Signed-off-by: corbolais <corbolais@gmail.com>

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,40 +1,40 @@
-#/bin/bash
+#!/bin/bash
 
 ###Updates pivpn scripts (Not PiVPN)
 ###Main Vars
 pivpnrepo="https://github.com/pivpn/pivpn.git"
 pivpnlocalpath="/etc/.pivpn"
-pivpnscripts="/opt/pivpn/scripts"
-bashcompletiondir="/etc/bash_completion.d/pivpn"
+pivpnscripts="/opt/pivpn/"
+bashcompletiondir="/etc/bash_completion.d/"
 
 
 ###Functions
 ##Updates scripts
 updatepivpnscripts(){
-    ##We don't know what sort of changes users have made. 
+    ##We don't know what sort of changes users have made.
     ##Lets remove first /etc/.pivpn dir then clone it back again
     echo "going do update PiVPN Scripts"
-    if [[ -d $pivpnlocalpath ]]; then
-
-        sudo rm -rf $pivpnlocalpath
+    if [[ -d "$pivpnlocalpath" ]]; then
+      if [[ -n "$pivpnlocalpath" ]]; then
+        sudo rm -rf "${pivpnlocalpath}/../.pivpn"
         cloneandupdate
-
+      fi
     else
         cloneandupdate
     fi
     echo "PiVPN Scripts have been updated"
 }
 
-##Updates scripts using test branch 
+##Updates scripts using test branch
 updatefromtest(){
-    ##We don't know what sort of changes users have made. 
+    ##We don't know what sort of changes users have made.
     ##Lets remove first /etc/.pivpn dir then clone it back again
     echo "PiVPN Scripts updating from test branch"
-    if [[ -d /etc/.pivpn ]]; then
-
-        rm -rf /etc/.pivpn
+    if [[ -d "$pivpnlocalpath" ]]; then
+      if [[ -n "$pivpnlocalpath" ]]; then
+        rm -rf "{$pivpnlocalpath}/../.pivpn"
         cloneupdttest
-
+      fi
     else
 
         cloneupdttest
@@ -46,9 +46,9 @@ updatefromtest(){
 ##Clone and copy pivpn scripts to /op/
 cloneandupdate(){
 
-        sudo git clone $pivpnrepo $pivpnlocalpath
-        sudo cp -r $pivpnlocalpath/scripts $pivpnscripts
-        sudo cp $pivpnlocalpath/scripts/bash-completion $bashcompletiondir
+        sudo git clone "$pivpnrepo" "$pivpnlocalpath"
+        sudo cp "${pivpnlocalpath}"/scripts/*.sh "$pivpnscripts"
+        sudo cp "${pivpnlocalpath}"/scripts/bash-completion "$bashcompletiondir"
 
 }
 
@@ -56,17 +56,18 @@ cloneandupdate(){
 ##and falls back to master branch again after updating
 cloneupdttest(){
 
-        sudo git clone $pivpnrepo $pivpnlocalpath
-        sudo git -C $pivpnlocalpath checkout test
-        sudo git -C $pivpnlocalpath pull origin test
-        sudo cp -r $pivpnlocalpath/scripts $pivpnscripts
-        sudo cp $pivpnlocalpath/scripts/bash-completion $bashcompletiondir
-        sudo git -C $pivpnlocalpath checkout master
+        sudo git clone "$pivpnrepo" "$pivpnlocalpath"
+        sudo git -C "$pivpnlocalpath" checkout test
+        sudo git -C "$pivpnlocalpath" pull origin test
+        sudo cp "${pivpnlocalpath}"/scripts/*.sh "$pivpnscripts"
+        sudo cp "${pivpnlocalpath}"/scripts/bash-completion "$bashcompletiondir"
+        sudo git -C "$pivpnlocalpath" checkout master
 
 }
 
 scriptusage(){
-    echo -e "Updates pivpn scripts,\n
+    echo -e "Updates pivpn scripts,
+
               Usage:
               pivpn update              | updates from master branch
               pivpn update -t or --test | updates from test branch"
@@ -81,16 +82,16 @@ if [[ $# -eq 0 ]]; then
 else
   while true; do
     case "$1" in
-      -t | --test | test )
+      -t|--test|test)
           updatefromtest
           exit 0
           ;;
-      -h | --help | help )
+      -h|--help|help)
           scriptusage
           exit 0
           ;;
-      * ) 
-        updatepivpnscripts 
+      * )
+        updatepivpnscripts
         exit 0
         ;;
     esac
