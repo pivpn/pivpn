61eaa7fd11de8e1aa8e6e6dfacc10b4cdae00879
Alex Povel <mail@alexpovel.de>
2020-03-03 17:03:05 +0100

    Fix typo and grammar

--- auto_install/install.sh
+++ auto_install/install.sh
@@ -1024,7 +1024,7 @@ askWhichVPN(){
 			fi
 		fi
 	else
-		chooseVPNCmd=(whiptail --backtitle "Setup PiVPN" --title "Installation mode" --separate-output --radiolist "WireGuard is a new kind of VPN that provides near-istantaneous connection speed, high performance, modern cryptography.\\n\\nIt's the recommended choice expecially if you use mobile devices where WireGuard is easier on battery than OpenVPN.\\n\\nOpenVPN is still available if you need the traditional, flexible, trusted VPN protocol. Or if you need features like TCP and custom search domain.\\n\\nChoose a VPN (press space to select):" "${r}" "${c}" 2)
+		chooseVPNCmd=(whiptail --backtitle "Setup PiVPN" --title "Installation mode" --separate-output --radiolist "WireGuard is a new kind of VPN that provides near-istantaneous connection speed, high performance and modern cryptography.\\n\\nIt's the recommended choice especially if you use mobile devices, where WireGuard is easier on battery than OpenVPN.\\n\\nOpenVPN is still available if you need the traditional, flexible, trusted VPN protocol. Or if you need features like TCP and custom search domain.\\n\\nChoose a VPN (press space to select):" "${r}" "${c}" 2)
 		VPNChooseOptions=(WireGuard "" on
 							OpenVPN "" off)
 
