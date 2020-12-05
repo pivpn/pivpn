308affe4e9aa69c66260026a00029c5f78c7d4e9
Orazio <orazioedoardo@users.noreply.github.com>
2020-12-05 12:24:22 +0100

    Workaround for the following error on Ubuntu 20.04:
      - /usr/bin/debconf-apt-progress: can't open /tmp/tmp.0CoNypDEPj: Permission denied at /usr/bin/debconf-apt-progress line 249, <STDIN> line 3.
        Reason: https://askubuntu.com/questions/1250974/user-root-cant-write-to-file-in-tmp-owned-by-someone-else-in-20-04-but-can-in

--- auto_install/install.sh
+++ auto_install/install.sh
@@ -546,7 +546,7 @@ installDependentPackages(){
 		fi
 	done
 
-	local APTLOGFILE="$(mktemp)"
+	local APTLOGFILE="$($SUDO mktemp)"
 
 	if command -v debconf-apt-progress > /dev/null; then
         # shellcheck disable=SC2086
