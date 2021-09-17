1423f973767b8c2e9256331aa672a29c85cd4498
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2021-09-17 16:05:45 +0100

    Fix grammar
    Update "simpling" to "simply".

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -241,7 +241,7 @@ removeAll(){
 
 	echo ":::"
 	printf "::: Finished removing PiVPN from your system.\\n"
-	printf "::: Reinstall by simpling running\\n:::\\n:::\\tcurl -L https://install.pivpn.io | bash\\n:::\\n::: at any time!\\n:::\\n"
+	printf "::: Reinstall by simply running\\n:::\\n:::\\tcurl -L https://install.pivpn.io | bash\\n:::\\n::: at any time!\\n:::\\n"
 }
 
 askreboot(){
