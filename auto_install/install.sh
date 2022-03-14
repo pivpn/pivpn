55047397ecf757e2e2247df71e274e0d1c7a8200
Normanras <44226464+Normanras@users.noreply.github.com>
2022-03-14 07:30:34 -0400

    while loop not finished

--- auto_install/install.sh
+++ auto_install/install.sh
@@ -1101,23 +1101,22 @@ setWireguardDefaultVars(){
 		pivpnDEV="wg0"
 		# Allow user to select a custom IP network for the setupVARS file, wg0. They can select optional, if they would like, or use the default of 10.6.0.0.
 		while [ -z "$pivpnNET" ]; do
-			if (whiptail --yesno --backtitle "Setup PiVPN" --title "Installation Mode" "Would you like to select your own IP network for your VPN? If you are not sure, please select no! Selecting yes may cause disruptions with your current network and is for advanced users only." 12 78 ); then 
+			if ( whiptail --yesno --backtitle "Setup PiVPN" --title "Installation Mode" "Would you like to select your own IP network for your VPN? Format must be x.x.0.0. WARNING: Choosing your own may cause disruptions with your current network and is for advanced users only." --yes-box "Select my own" --no-box "Use Default" 20 78 ); then 
 				answer=$(whiptail --backtitle "Setup PiVPN" --title "Installation Mode" --inputbox "Please Choose your IP Gateway Address" 8 78 3>&1 1>&2 2>&3) 
 					if [[ $answer =~ ^([0-9]{1,3}\.){2}[0]{1,3}\.[0]{1,3}$ ]]; then
 						whiptail --msgbox --backtitle "Setup PiVPN" --title "Installation Mode" "$answer is a valid IP!" 8 78
 						pivpnNET=$answer
 					else
-						if (whiptail --yesno --backtitle "Setup PiVPN" --title "Installation Mode" "$answer is not a valid IP! Select yes to Try again, no to use the default of 10.6.0.0" 8 78); then
+						if ( whiptail --yesno --backtitle "Setup PiVPN" --title "Installation Mode" "$answer is not a valid IP! Select yes to Try again, no to use the default of 10.6.0.0" 8 78); then
 							continue	
 						else	
 							whiptail --msgbox --title "DEFAULT" "Great! Using the default IP of 10.6.0.0" 8 78
 							pivpnNET="10.6.0.0"
 						fi
-					fi
 			else 
 				continue
 			fi
-		done
+		done		
 		vpnGw="${pivpnNET/.0.0/.0.1}"
 		# Allow custom allowed IPs via unattend setupVARs file. Use default if not provided.
 		if [ -z "$ALLOWED_IPS" ]; then
