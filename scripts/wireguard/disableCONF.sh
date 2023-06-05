4b6ae387b79a4a2aa8e9629bacb8efcd732527e4
Dolorosus <github@dolorosus.de>
2023-06-05 13:11:30 +0200

    Unique identification of client names in pivp -on/-off

--- scripts/wireguard/disableCONF.sh
+++ scripts/wireguard/disableCONF.sh
@@ -97,7 +97,7 @@ for CLIENT_NAME in "${CLIENTS_TO_CHANGE[@]}"; do
 
   if ! grep -q "^${CLIENT_NAME} " configs/clients.txt; then
     echo -e "::: \e[1m${CLIENT_NAME}\e[0m does not exist"
-  elif grep -q "#\[disabled\] ### begin ${CLIENT_NAME}" wg0.conf; then
+  elif grep -q "#\[disabled\] ### begin ${CLIENT_NAME} ###" wg0.conf; then
     echo -e "::: \e[1m${CLIENT_NAME}\e[0m is already disabled"
   else
     if [[ -n "${CONFIRM}" ]]; then
